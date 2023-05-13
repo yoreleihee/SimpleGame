5914ad8984cc54fa3b5713731345af6d59dd8d49
Huiji_MacMini <gmlwl0408@gmail.com>
2023-05-13 17:03:12 +0900

    [Feat] 플레이어가 좌, 우로 움직인다.

---
+++ SoEasyGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Vector3 _moveDirection;
+    [SerializeField] private float _moveSpeed = 4f;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _rigidbody.velocity = _moveDirection * _moveSpeed;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+        if (input != null)
+        {
+            _moveDirection = new Vector3(input.x, 0f, 0f);
+        }
+    }
+}
