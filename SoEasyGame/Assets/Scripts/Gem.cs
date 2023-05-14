f04a3e4e96bee940471442c9454ad16c1d1ca72e
Huiji_MacMini <gmlwl0408@gmail.com>
2023-05-15 02:53:57 +0900

    [Feat] Gem 풀링

--- SoEasyGame/Assets/Scripts/Gem.cs
+++ SoEasyGame/Assets/Scripts/Gem.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Literal;
@@ -9,11 +8,20 @@ public class Gem : MonoBehaviour
 {
     [SerializeField] private float _points;
     [SerializeField] private float _gaugeValue;
+
+    private Stack<Gem> _stack;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(TagString.PLAYER))
         {
             GetScoreAndGauge();
+            _stack.Push(this);
+            gameObject.SetActive(false);
+        }
+
+        if (other.CompareTag(TagString.BOUNDARY))
+        {
+            _stack.Push(this);
             gameObject.SetActive(false);
         }
     }
@@ -22,5 +30,11 @@ public class Gem : MonoBehaviour
     {
         UIModel.Score += _points;
         UIModel.Gauge += _gaugeValue;
+    }
+
+    // 어떤 스택에서 나왔는지 알려준다
+    public void SetStackReference(Stack<Gem> stack)
+    {
+        _stack = stack;
     }
 }
--- SoEasyGame/Assets/Scripts/System/SpawnManager.cs
+++ SoEasyGame/Assets/Scripts/System/SpawnManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using Literal;
@@ -7,23 +8,43 @@ using Random = UnityEngine.Random;
 public class SpawnManager : MonoBehaviour
 {
     private Gem _gem;
+    private Stack<Gem> _gemStack;
+    [SerializeField] private int _numberOfGemsToCreate = 6;
+    private float[] _xPos = { -5f, 0f, 5f };
 
     private void Start()
     {
+        _gemStack = new Stack<Gem>();
         _gem = Resources.Load<Gem>(PathString.GEM_FILE_PATH);
+        
+        int randomIndex = Random.Range(0, _xPos.Length);
+        float randomX = _xPos[randomIndex];
+        
+        for (int i = 0; i < _numberOfGemsToCreate; ++i)
+        {
+            Gem gem = Instantiate(_gem, new Vector3(randomX, transform.position.y, transform.position.z), _gem.transform.rotation);
+            
+            gem.gameObject.SetActive(false);
+            _gemStack.Push(gem);
+        }
+        
         SpawnRoutine().Forget();
     }
 
-    private float[] _xPos = { -5f, 0f, 5f };
     private async UniTaskVoid SpawnRoutine()
     {
         while (true)
         {
             await UniTask.Delay(TimeSpan.FromSeconds(2f));
-            int randomIndex = Random.Range(0, _xPos.Length);
-            float randomX = _xPos[randomIndex];
-            Instantiate(_gem, new Vector3(randomX, transform.position.y, transform.position.z), _gem.transform.rotation);
+            GetGemFromStack();
         }
     }
-    
+
+    private void GetGemFromStack()
+    {
+        Gem gem = _gemStack.Pop();
+        // 스택 참조 연결
+        gem.gameObject.SetActive(true);
+        gem.SetStackReference(_gemStack);
+    }
 }
